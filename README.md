0335ef0245188fc12cda0fdec66534cd863dab41
unknown <hiurimi@gmail.com>
2023-07-28 13:07:28 -0400

    finish product

---
+++ README.md
@@ -0,0 +1,16 @@
+Word Scramble Game 
+
+The word scramble game uses HTML, CSS, and JavaScript. Players are to guess the correct word from a scrambled word pulled from a const array. Hints are provided for each. Your opponent is the computer. For each word, there are three attempts to guess word correctly before losing the game. After 6 successful guesses, you win.  
+
+How to Play
+Enter your guess in the input field and click the "Guess" button.
+If correct, a new scramble word will appear.
+Otherwise, you have two more attempts to guess the word correctly.
+After 6 words correctly, you win the game and can start over
+after three attempts, the game is over, and you can start over by clicking the "Start Over" button.
+The game includes a list of 10 words and hints. An array list was used. 
+
+
+Technologies Used
+HTML, CSS, JavaScript
+replit and VScode
---
+++ script.js
@@ -0,0 +1,103 @@
+const words = [
+  { word: "mango", hint: "It's a tropical fruit." },
+  { word: "elephant", hint: "It's the largest land animal." },
+  { word: "giraffe", hint: "It has a long neck." },
+  { word: "panda", hint: "It's black and white." },
+  { word: "koala", hint: "It's a marsupial from Australia." },
+  { word: "zebra", hint: "It has black and white stripes." },
+  { word: "boat", hint: "It sails." },
+  { word: "spoon", hint: "You eat with it." },
+  { word: "apple", hint: "Fall favorite for picking." },
+  { word: "tiger", hint: "It's a fierce predator." },
+];
+
+let currentRound = 0;
+let triesLeft = 3;
+const roundsToWin = 6;
+
+const scrambledWordE = document.getElementById("scrambledWord");
+const hintE = document.getElementById("hint");
+const triesLeftE = document.getElementById("triesLeft");
+const guessInputE = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessButton");
+const messageE = document.getElementById("message");
+const startOverButton = document.getElementById("startOverButton");
+const winMessage = document.getElementById("winMessage");
+
+const titleM = document.querySelector('h1');
+titleM.style.color = 'purple';
+hintE.style.color = 'purple';
+triesLeftE.style.color = 'red';
+guessButton.style.color = 'green';
+
+function scrambleWord(word) {
+  let scrambled = word.split("").sort(() => Math.random() - 0.5).join("");
+  return scrambled;
+}
+
+function displayScrambledWord() {
+  let currentWord = words[currentRound];
+  let scrambledWord = scrambleWord(currentWord.word);
+  scrambledWordE.textContent = scrambledWord;
+  hintE.textContent = `Hint: ${currentWord.hint}`;
+  scrambledWordE.style.color = 'red';
+}
+
+function startRound() {
+  winMessage.style.display = "none"; /* Hide "Smarty!" message at the start */
+  if (currentRound < words.length) {
+    displayScrambledWord();
+    guessButton.disabled = false;
+    guessInputE.value = "";
+    messageE.textContent = "";
+    triesLeft = 3;
+    triesLeftE.textContent = triesLeft;
+    startOverButton.disabled = true;
+  } else {
+    /* Player wins */
+    winMessage.style.display = "block"; /* Show "Smarty!" */
+    guessButton.disabled = true;
+    guessInputE.disabled = true;
+    startOverButton.disabled = false;
+  }
+}
+
+function checkGuess() {
+  let currentWord = words[currentRound];
+  let guess = guessInputE.value.trim().toLowerCase();
+  if (guess === currentWord.word) {
+    currentRound++;
+    if (currentRound >= roundsToWin) {
+      /* Player wins after 6*/
+      winMessage.style.display = "block";
+      guessButton.disabled = true;
+      guessInputE.disabled = true;
+      startOverButton.disabled = false;
+    } else {
+      /*  next round */
+      messageE.textContent = "Correct!";
+      startRound();
+    }
+  } else {
+    triesLeft--;
+    triesLeftE.textContent = triesLeft;
+    if (triesLeft <= 0) {
+      messageE.textContent = "Game Over! Try again!";
+      guessButton.disabled = true;
+      startOverButton.disabled = false;
+    } else {
+      messageE.textContent = "Ooops! Try again.";
+    }
+  }
+}
+
+function startOver() {
+  currentRound = 0;
+  winMessage.style.display = "none";
+  startRound();
+}
+
+guessButton.addEventListener("click", checkGuess);
+startOverButton.addEventListener("click", startOver);
+
+startRound();
